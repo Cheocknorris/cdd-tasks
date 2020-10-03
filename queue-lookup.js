c8a1ea1fe5d0ac991be2e2c6fe6db97bdc6660b2
Ravi Sharma <ravisharma1987@gmail.com>
2020-10-03 12:45:52 +0200

    review

--- queue-lookup.js
+++ queue-lookup.js
@@ -1,57 +1,59 @@
 class Queue {
-    constructor() {
-      this.store = new Array(3);
-      this.front = 0;
-      this.rear = 0;
+  constructor() {
+    this.store = new Array(3);
+    this.front = 0;
+    this.rear = 0;
+  }
+
+  enqueue(value) {
+    if (this.rear === this.store.length) return null; // full
+
+    this.store[this.rear] = value;
+    this.rear++;
+    return true;
+  }
+
+  dequeue() {
+    if (this.isEmpty()) return null;
+
+    let valueAtFront = this.store[this.front];
+    this.front++;
+    if (this.front == this.rear) {
+      this.front = this.rear = 0;
     }
-  
-    enqueue(value) {
-      if (this.rear === this.store.length) return null; // full
-  
-      this.store[this.rear] = value;
-      this.rear++;
-      return true;
-    }
-  
-    dequeue() {
-      if (this.isEmpty()) return null;
-  
-      let valueAtFront = this.store[this.front];
-      this.front++;
-      if (this.front == this.rear) {
-        this.front = this.rear = 0;
-      }
-      return valueAtFront;
-    }
-  
-    lookUp(value) {
-        if (!value) {
-            throw new Error('Invalid input');
-        } 
-        if (this.isEmpty()) return false; 
-        for (let i = 0; i < this.store.length; i++) {
-            if (this.store[i] === value) {
-                return true;
-            }
-        }
-        return false;
+    return valueAtFront;
+  }
+
+  lookUp(value) {
+    if (!value) {
+      throw new Error('Invalid input');
     }
 
-    peek() {
-      if (this.isEmpty()) return null;
-  
-      return this.store[this.front];
+    if (this.isEmpty()) return -1;
+
+    for (let i = 0; i < this.store.length; i++) {
+      if (this.store[i] === value) {
+        return i;
+      }
     }
-  
-    isEmpty() {
-      return this.front == this.rear;
-    }
+    return -1;
   }
-  
-  
-  let queue = new Queue();
-  console.log(queue.isEmpty(), true);
-  console.log(queue.enqueue(5), true);
-  console.log(queue.enqueue(6), true);
-  console.log(queue.enqueue(7), true);
-  console.log('lookUp: ', queue.lookUp());
+
+  peek() {
+    if (this.isEmpty()) return null;
+
+    return this.store[this.front];
+  }
+
+  isEmpty() {
+    return this.front == this.rear;
+  }
+}
+
+
+let queue = new Queue();
+console.log(queue.isEmpty(), true);
+console.log(queue.enqueue(5), true);
+console.log(queue.enqueue(6), true);
+console.log(queue.enqueue(7), true);
+console.log('lookUp: ', queue.lookUp(5));
--- stack-lookup.js
+++ stack-lookup.js
@@ -3,12 +3,12 @@ class Stack {
       this.store = new Array(1000);
       this.stackPointer = -1;
     }
-  
+
     push(value) {
       this.stackPointer++;
       this.store[this.stackPointer] = value;
     }
-  
+
     pop() {
       if (this.isEmpty()) return null;
       let topElement = this.store[this.stackPointer--];
@@ -18,27 +18,28 @@ class Stack {
     lookUp(value) {
         if (!value) {
             throw new Error('Invalid input');
-        } 
-        if (this.isEmpty()) return false;
+        }
+        if (this.isEmpty()) return -1;
+
         for (let i = 0; i < this.store.length; i++) {
             if (this.store[i] === value) {
-                return true
+                return -1
             }
         }
         return false;
     }
-    
+
     isEmpty() {
       return this.stackPointer < 0;
     }
-  
+
     peek() {
       if (this.isEmpty()) return null;
-  
+
       return this.store[this.stackPointer];
     }
   }
-  
+
   let stack = new Stack();
   console.log(stack.isEmpty(), 'true');
   console.log(stack.peek(), 'null');
