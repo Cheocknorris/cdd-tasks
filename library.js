bf058b3dcb5c006e2efed6443ead0e75e6e6b643
Eliseo Quintanilla <eliseo.quintanillagz@gmail.com>
2020-10-25 20:30:10 +0100

    user class up and running

--- library.js
+++ library.js
@@ -16,19 +16,6 @@ class Book {
     }
 }
 
-const bookOne = new Book(1, 'Book One', 'Author One', 'available', true);
-const bookTwo = new Book(2, 'Book Two', 'Author Two', 'available', false);
-const bookThree = new Book(3, 'Book Three', 'Author Three', 'available', true);
-const bookFour = new Book(4, 'Book Four', 'Author Four', 'available', true);
-const bookFive = new Book(5, 'Book Five', 'Author Five', 'reference', true);
-const bookSix = new Book(6, 'Book Six', 'Author Six', 'available', true);
-const bookSeven = new Book(7, 'Book Seven', 'Author Seven', 'reference', true);
-const bookEight = new Book(8, 'Book Eight', 'Author Eight', 'available', true);
-const bookNine = new Book(9, 'Book Nine', 'Author Nine', 'available', true);
-const bookTen = new Book(10, 'Book Ten', 'Author Ten', 'available', true);
-const bookEleven = new Book(11, 'Book Eleven', 'Author Eleven', 'available', true);
-const bookTwelve = new Book(12, 'Book Twelve', 'Author Twelve', 'available', true);
-
 class BookShelf {
     constructor(bookShelfId, size) {
         this.bookShelfId = bookShelfId;
@@ -51,6 +38,19 @@ class BookShelf {
     }
 
     searchByTitle(title) {
+        if (typeof title !== 'string') throw new Error('Invalid input')
+        if (this.isEmpty()) return false;
+        
+        const shelfCopy = [...this.storage];
+        
+        for (let i = 0; i < shelfCopy.length; i++) {
+            if (shelfCopy[i].title === title) return shelfCopy[i];
+        }
+
+        return false;
+    }
+
+    locateInShelf(title) {
         if (typeof title !== 'string') throw new Error('Invalid input')
         if (this.isEmpty()) return false;
         
@@ -106,18 +106,77 @@ class Library {
     }
 
     searchByTitle(title) {
+        try {
+            for (let i = 0; i < this.shelves.length; i++) {
+                if (this.shelves[i].searchByTitle(title)) {
+                    return this.shelves[i].searchByTitle(title);
+                } 
+            }
+            throw new Error('Book not found');
+        } catch(e) {
+            console.log(e)
+        }
+    }
+
+    locateInShelf(title) {
         for (let i = 0; i < this.shelves.length; i++) {
-            if (this.shelves[i].searchByTitle(title) !== false) {
+            if (this.shelves[i].locateInShelf(title)) {
                 let result = { 
                     shelf: i + 1,
-                    index: this.shelves[i].searchByTitle(title).index,
-                    book: this.shelves[i].searchByTitle(title).book,
+                    index: this.shelves[i].locateInShelf(title).index,
+                    book: this.shelves[i].locateInShelf(title).book,
                 }
                 return result;
             }  
         }
+        throw new Error('Book not found');
     }
 }
+
+
+class User {
+    constructor(userId, name) {
+        this.userId = userId;
+        this.name = name;
+        this.booksBorrowed = new Set();
+    }
+
+    issueBook(title) {
+        try {
+            if (library.searchByTitle(title) 
+            && library.searchByTitle(title).status === 'available') {
+            this.booksBorrowed.add(library.searchByTitle(title));
+            library.searchByTitle(title).markAsIssued(this.name);
+            } 
+        } catch(e) {
+            console.log(e);
+        }   
+    }
+
+    returnBook(title) {
+        if (library.searchByTitle(title)) {
+            this.booksBorrowed.delete(library.searchByTitle(title));
+            library.searchByTitle(title).markAsAvailable();
+        }
+    } 
+
+}
+
+
+
+const bookOne = new Book(1, 'Book One', 'Author One', 'available', true);
+const bookTwo = new Book(2, 'Book Two', 'Author Two', 'available', false);
+const bookThree = new Book(3, 'Book Three', 'Author Three', 'available', true);
+const bookFour = new Book(4, 'Book Four', 'Author Four', 'available', true);
+const bookFive = new Book(5, 'Book Five', 'Author Five', 'reference', true);
+const bookSix = new Book(6, 'Book Six', 'Author Six', 'available', true);
+const bookSeven = new Book(7, 'Book Seven', 'Author Seven', 'reference', true);
+const bookEight = new Book(8, 'Book Eight', 'Author Eight', 'available', true);
+const bookNine = new Book(9, 'Book Nine', 'Author Nine', 'available', true);
+const bookTen = new Book(10, 'Book Ten', 'Author Ten', 'available', true);
+const bookEleven = new Book(11, 'Book Eleven', 'Author Eleven', 'available', true);
+const bookTwelve = new Book(12, 'Book Twelve', 'Author Twelve', 'available', true);
+
 
 const library = new Library();
 
@@ -133,26 +192,12 @@ console.log(library.addBook(bookNine), true);
 console.log(library.addBook(bookTen), true);
 console.log(library.addBook(bookEleven), true);
 console.log(library.addBook(bookTwelve), true);
+console.log(library.locateInShelf('Book One'));
 console.log(library.searchByTitle('Book One'));
 
-
-class User {
-    constructor(userId, name) {
-        this.userId = userId;
-        this.name = name;
-        this.booksBorrowed = new Set();
-    }
-
-    borrowBook(title) {
-        let borrowedBook = library.searchByTitle(title).book;
-        this.booksBorrowed.add(borrowedBook);
-        
-        let name = this.name;
-
-        library.searchByTitle(title).markAsIssued(name);
-    }
-}
-
 const userOne = new User(1, 'User One');
-userOne.borrowBook('Book One');
+userOne.issueBook('Book One');
+userOne.issueBook('Book Twelve');
+userOne.returnBook('Book One');
 console.log(userOne);
+console.log(library.searchByTitle('Book One'));
